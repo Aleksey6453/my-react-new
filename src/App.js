a446d4a2ef6575bfb8894110db3d7e94869bb292
login6453 <login6453@ukr.net>
2019-04-05 00:22:32 +0300

    add new input and btn for test

--- src/App.js
+++ src/App.js
@@ -1,127 +1,45 @@
 import React, { Component } from 'react';
 import './App.css';
-// import InputNew from "./inputFolder/InputMy";
-// import ButtonNew from "./buttonFolder/ButtonMy";
-import TitleNew from "./titleFolder/TitleMy"
-import Cyborg from "./cyborg/Cyborg"
-import InputMy from "./inputFolder/InputMy.css"
-import TodoList from "./testFolder/TodoList"
-import TodoForm from "./testFolder/TodoForm"
-import NEW from "./newFolder/TodoNewL"
+
 
 
 
 
 class App extends Component {
- 
- 
-  state = {
-    // cyborgs : [
-    //   {model: "Optimus", number: "1325", art: "a"},
-    //   {model: "Optimus22", number: "1326", art: "h"},
-    //   {model: "Optimus33", number: "1327", art: "z"}
-    // ],
 
-    // pageTitle : "React Insane",
-    // showCyborgs : false
-
+  state={
+    todos: [],
+    curent_todo: ""
   }
 
-  clickClack = (event) => {
-    alert("Click Clack")
-  }
-
-  changeHandle = (newTitle) => {
-      this.setState({
-        pageTitle: newTitle
-      })
-  } 
-
-  toggleCyborgsHendler = () => {
-    console.log("TTTOOOGGLLEEE")
+  changeInput = (event) => {
     this.setState({
-      showCyborgs : !this.state.showCyborgs
+      curent_todo: event.target.value
     })
   }
 
-  deleteHandler = (i) => {
-    console.log("Del")
-    // this.setState({pageTitle:"DElEtEd"}) 
-    const cyborgs = this.state.cyborgs.concat()
-    cyborgs.splice(i, 1)
-
-    this.setState({cyborgs})
+  putTodo = () => {
+    let z = this.state.todos.slice();
+    z.push(this.state.curent_todo);
+    this.setState({
+      todos: z,
+      curent_todo: ""
+    })
   }
 
-  newNewZ = () => {
-    alert("ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ")
-  }
-
-  // handleInput = (event) => {
-  //   this.setState({
-  //     pageTitle : event.target.value
-  //   })
-   
-  // }
-
-  handleClick = () => {
-    console.log("button input works!")
-    
-  }
-
-  onChangeName = (model, i) => {
-     const cyborg = this.state.cyborgs[i]
-     cyborg.model = model
-     const cyborgs = [...this.state.cyborgs]
-     cyborgs[i] = cyborg 
-     this.setState({cyborgs})
-  }
-
-  render() {
-
-    let cyborgs = null
-    if(this.state.showCyborgs){
-      cyborgs =  this.state.cyborgs.map((cyborg, i) => {
-        return (
-          <Cyborg 
-             key = {i}
-             model= {cyborg.model}
-             number = {cyborg.number}
-             art = {cyborg.art}
-             onDelete = {this.deleteHandler.bind(this,i)}
-             onChangeName = {(event) => { this.onChangeName( event.target.value, i)}}
-          />
-        )
-      })
-    }
-
-   return (
-
+ 
+ render() {
+    return (
       
       <div className="App">
         <div className="formNew">
-            <form action="#" >
+           <div className="flexRow">
+
+             <input type="text" placeholder="enter todo..." onChange={this.changeInput} value={this.state.curent_todo} className="inputNewClass"/>
+             <button className="btnNew" onClick={this.putTodo}>Add todo</button>
+           </div>
                
-              <TitleNew />
-              {/* <p>{this.state.pageTitle}</p>  */}
-              {/* <button className="btnNew" onClick={this.toggleCyborgsHendler}>Show on/off</button> */}
-              
-              <div className="flexRow">
-                  {/* <InputNew /> */}
-                  {/* <input type="text"  onChange={this.handleInput} className="inputNewClass" name="text" placeholder="add text please..."/> */}
-                  {/* <ButtonNew /> */}
-                  {/* <button className="btnNew btnRound" onClick={this.changeHandle.bind(this, "Check")}>Add</button> */}
-              </div>
-
-              { cyborgs }
-              
-              <TodoList />
-              <NEW />
-
-              
-              
-
-            </form>
+           
         </div>
          
         
